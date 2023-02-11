f04703c16b3dc7c000a13588e612a25032422f88
Pablo Lorenzo <pablo.lorenzo@constellaintelligence.com>
2023-02-11 13:18:35 +0100

    fixing paths

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,13 @@
 const { loadEnvConfig } = require('@next/env')
 
 const dev = process.env.NODE_ENV !== 'production'
-
+const path = require('path');
+console.log(path.resolve(__dirname, '../../../'));
 module.exports = {
   client: 'sqlite3', // or 'better-sqlite3'
   useNullAsDefault: true,
   connection: {
-    filename: "./mydb.sqlite"
+    filename: path.resolve(__dirname, '../../../mydb.sqlite')
   },
   migrations: {
     directory: './knex/migrations',
